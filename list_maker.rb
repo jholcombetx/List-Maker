4acd7b6feda5d551548c8b72ebeec461fc78eae8
James Holcombe <jholcombeihs@gmail.com>
2016-07-22 17:09:58 -0500

    update2

--- list_maker.rb
+++ list_maker.rb
@@ -1,27 +1,36 @@
-puts "Welcome to the JGH List Maker: " +
-"add items by entering the name of the item and pressing enter, " +
-"or enter 'total' to see your full list, " +
-"or enter 'not' (no spaces) at the front of an item you've entered "+
-"to remove that item from your list," +
-"or enter 'exit' to exit. Thanks for trying me out!"
+def greeting
+	puts "Welcome to the JGH List Maker: " +
+	"add items by entering the name of the item and pressing enter."
+	puts "Enter 'total' to see your full list. "
+	puts "Enter 'not' at the front of an item you've already entered, and hit 'enter' to remove that item from the list"
+	puts "Enter 'help' for a reminder of how this app works"
+	puts "Enter 'sort' to alphabetize your list."
+	puts "Enter 'clear' to clear your current list"
+	puts "Enter 'exit' when you're finished. "
+end
 
 def game 
 	answer = false
 	my_array =  []
 	while not answer
 		item = gets.chomp.downcase
-			if item == 'exit'
-				puts "Thanks for using my to-do list"
-				answer = true
+			if item == 'help'
+				greeting
 			elsif item == 'total'
 				puts "Your To-do list consists of the following: " + my_array.to_s
 			elsif item.include?"not"
 				item[0,3]=''
 				my_array.delete(item)
+			elsif item == 'sort'
+				my_array.sort!
+				puts "Your To-do list consists of the following: " + my_array.to_s
+			elsif item == 'clear'
+				my_array.clear
 			else
 				my_array << item
 			end 
 	end
 end
 
+greeting
 game
